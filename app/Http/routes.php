b747b8937ef89b932238405c3de591e1c90999c4
Rahulsss443 <Rahulsss443@gmail.com>
2016-09-04 23:21:04 +0530

    Basic setup (#1)
    * migrations
    
    * delete unwanted files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,39 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+/**
+ * Routes for resource fight-controller
+ */
+$app->get('fight-controller', 'FightControllersController@all');
+$app->get('fight-controller/{id}', 'FightControllersController@get');
+$app->post('fight-controller', 'FightControllersController@add');
+$app->put('fight-controller/{id}', 'FightControllersController@put');
+$app->delete('fight-controller/{id}', 'FightControllersController@remove');
+
+/**
+ * Routes for resource test
+ */
+$app->get('test', 'TestsController@all');
+$app->get('test/{id}', 'TestsController@get');
+$app->post('test', 'TestsController@add');
+$app->put('test/{id}', 'TestsController@put');
+$app->delete('test/{id}', 'TestsController@remove');
+
+/**
+ * Routes for resource example
+ */
+$app->get('example', 'Example@all');
+$app->get('example/{id}', 'Example@get');
+$app->post('example', 'Example@add');
+$app->put('example/{id}', 'Example@put');
+$app->delete('example/{id}', 'Example@remove');
+
+/**
+ * Routes for resource example-controller
+ */
+$app->get('example-controller', 'ExampleController@all');
+$app->get('example-controller/{id}', 'ExampleController@get');
+$app->post('example-controller', 'ExampleController@add');
+$app->put('example-controller/{id}', 'ExampleController@put');
+$app->delete('example-controller/{id}', 'ExampleController@remove');
---
+++ database/migrations/2016_09_04_164419_setup_tables.php
@@ -0,0 +1,102 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SetupTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('uuid')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('fighters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('fights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('fighter1_id')->unsigned();
+            $table->integer('fighter2_id')->unsigned();
+            $table->foreign('fighter1_id')->references('id')->on('fighters')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('fighter2_id')->references('id')->on('fighters')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('rounds');
+            $table->timestamps();
+        });
+
+        Schema::create('outcomes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('description')->unique();
+            $table->string('abbr')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('fight-result', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('fight_id')->unsigned();
+            $table->foreign('fight_id')->references('id')->on('fights')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('round_no');
+            $table->integer('fighter1_score');
+            $table->integer('fighter2_score');
+            $table->integer('outcome_id')->unsigned();
+            $table->integer('winner')->unsigned();
+            $table->foreign('outcome_id')->references('id')->on('outcomes')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('winner')->references('id')->on('fighters')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('predictions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('user_prediction', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('fight_id')->unsigned();
+            $table->integer('prediction_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('fight_id')->references('id')->on('fights')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('prediction_id')->references('id')->on('predictions')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+        Schema::drop('fighters');
+        Schema::drop('fights');
+        Schema::drop('fight-result');
+        Schema::drop('predictions');
+        Schema::drop('user_prediction');
+    }
+}
